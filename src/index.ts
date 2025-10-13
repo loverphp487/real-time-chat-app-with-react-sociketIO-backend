6360028bbdad030db156e468530d291c09b1037c
loverphp <loverphp487@gmail.com>
2025-10-13 06:38:32 +0300

    add authentication to io

--- src/index.ts
+++ src/index.ts
@@ -13,20 +13,11 @@ import authRouter from '@/routes/auth.route';
 import userRoutes from '@/routes/user.route';
 import { Server as SocketIOServer } from 'socket.io';
 
-// import '@/config/passport-config';
 import chatRoute from '@/routes/chat.route';
-// import ConnectMongoDBSession from 'connect-mongodb-session';
-// import ExpressMongoSanitize from 'express-mongo-sanitize';
+
 import socketMiddleware, {
 	checkUserLoginWithSocket,
 } from './middlewares/socketMiddleware';
-
-// const MongoDBStore = ConnectMongoDBSession(session);
-
-// const store = new MongoDBStore({
-// 	uri: CONFIG.MONGO_URL as string,
-// 	collection: 'Sessions',
-// });
 
 const app: Express = express();
 
@@ -59,42 +50,6 @@ app.options(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cookieParser(CONFIG.SESSION_SECRET!));
-
-// app.use(
-// 	ExpressMongoSanitize({
-// 		replaceWith: '_',
-// 		/**
-// 		 * A function to be called when a NoSQL injection attempt is detected.
-// 		 * It takes an object with two properties: `req` which is the Express request object,
-// 		 * and `key` which is the key that was found to contain malicious input.
-// 		 * The function should log a warning message indicating the path of the request and the
-// 		 * key that was found to contain malicious input.
-// 		 */
-// 		onSanitize: ({ req, key }) => {
-// 			console.warn(`NoSQL Injection attempt at ${req.path}:`, key);
-// 		},
-// 	}),
-// );
-
-// app.use(
-// 	session({
-// 		secret: CONFIG.SESSION_SECRET as string, // Replace with a strong, randomly generated secret
-// 		resave: false,
-// 		saveUninitialized: false,
-// 		store: store,
-// 		cookie: {
-// 			secure: CONFIG.ENV_NODE === 'production', // Use secure cookies in production
-// 			sameSite: CONFIG.ENV_NODE === 'production' ? 'none' : 'lax', // Adjust based on deployment
-// 			maxAge: 1000 * 60 * 60 * 24, // 24 hours
-// 			httpOnly: true,
-// 		},
-// 	}),
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
 /**
  * apply cors with cors options
  */
@@ -109,7 +64,7 @@ app.get('/', (req, res) => {
 
 app.use(notFound);
 app.use(RequestHandlerError);
-// io.use(checkUserLoginWithSocket);
+io.use(checkUserLoginWithSocket);
 
 httpServer.listen(CONFIG.PORT || 3000, () => {
 	ConnectionToDatabase();
